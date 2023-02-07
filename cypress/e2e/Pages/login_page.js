ad96010f9aa8a2026ccee558d77468b9425ea29f
Leena Madhok <leenamadhok@Leenas-MacBook-Air.local>
2023-02-07 22:35:38 +0530

    POM part-4

--- cypress/e2e/Pages/login_page.js
+++ cypress/e2e/Pages/login_page.js
@@ -1,11 +1,16 @@
 export class LoginPage{
+
+    userName_input = ':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input'
+    userPassword_input = ':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input'
+    login_button = '.oxd-button'
+
     enterusername(name){
-        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(name)
+        cy.get(this.userName_input).type(name)
     }
     enterpassword(password){
-        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password)
+        cy.get(this.userPassword_input).type(password)
     }
     loginbutton(){
-        cy.get('.oxd-button').click()
+        cy.get(this.login_button).click()
     }
 }
